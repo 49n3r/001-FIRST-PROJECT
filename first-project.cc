672a521bdc5533b68190fe014d608219f7435289
Chinonso Ugwumadu <cu884120@ohio.edu>
2021-05-16 15:21:26 -0400

    Initial Commit

---
+++ first-project.cc
@@ -0,0 +1,22 @@
+/*
+   File:        PROGRAM-NAME.cc
+   Author:      UGWUMADU, Chinonso Ephraim
+   Date:        05/DAY/2021
+   Description: ENTER DESCRIPTION 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    string name = "Nonso";
+    int age; 
+    cout << "Enter Nonso age:: ";
+    cin >> age;
+    cout << "Hello Git!!" << endl 
+    << "My name is " << name << "\n"
+    << "I am " << age << " years old";
+    return 0;
+}//mainSS
